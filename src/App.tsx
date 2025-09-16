38e2ae5356593f4903ab5318aeab31921e29364a
dspedroborges <xpedrostewart@gmail.com>
2025-09-16 16:44:32 -0300

    pointers

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
 import { useState, useEffect } from "react";
 
-function App() {
-const [darkMode, setDarkMode] = useState(() => {
-  const saved = localStorage.getItem("darkMode");
-  return saved === "true" ? true : false;
-});
-
-useEffect(() => {
-  if (darkMode) {
-    document.documentElement.classList.add("dark");
-  } else {
-    document.documentElement.classList.remove("dark");
+const projects = [
+  {
+    title: "CRUD Helper",
+    description: "A React component library that simplifies CRUD operations. Easily create forms from JSON using the FormMaker component and display, update, or delete records with the Table component. Works seamlessly with Tailwind and Bootstrap. Developers only need to define fields and provide onUpdate and onDelete functions.",
+    demo: "https://crudhelper.vercel.app/",
+    github: "https://github.com/dspedroborges/crudhelper",
   }
-  localStorage.setItem("darkMode", JSON.stringify(darkMode));
-}, [darkMode]);
+];
+
+function App() {
+  const [darkMode, setDarkMode] = useState(() => {
+    const saved = localStorage.getItem("darkMode");
+    return saved === "true" ? true : false;
+  });
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+    localStorage.setItem("darkMode", JSON.stringify(darkMode));
+  }, [darkMode]);
   return (
     <div className="flex flex-col min-h-screen bg-gray-100 dark:bg-gray-900 md:flex-row transition-colors duration-300">
       <aside className="w-full bg-gray-800 text-white dark:bg-gray-950 dark:text-gray-200 flex flex-col p-6 md:w-64">
@@ -46,24 +55,30 @@ useEffect(() => {
         <section id="projects" className="mb-16 md:mb-20">
           <h2 className="text-3xl font-bold mb-4">Projects</h2>
           <div className="grid gap-6 sm:grid-cols-1 md:grid-cols-2">
-            <div className="bg-white dark:bg-gray-800 p-6 rounded shadow transition-colors duration-300">
-              <h3 className="text-xl font-semibold mb-2 dark:text-gray-200">CRUD Helper</h3>
-              <p className="text-gray-600 dark:text-gray-300">
-                A React component library that simplifies CRUD operations. Easily create forms from JSON using the FormMaker component and display, update, or delete records with the Table component. Works seamlessly with Tailwind and Bootstrap. Developers only need to define fields and provide onUpdate and onDelete functions.
-              </p>
-              <div className="flex items-center gap-2 mt-2 flex-wrap">
-                <a href="https://crudhelper.vercel.app/" target="_blank">
-                  <button className="p-2 my-2 rounded-xl bg-gray-900 dark:bg-gray-700 text-white hover:bg-gray-700 dark:hover:bg-gray-600 flex items-center gap-2 justify-center">
-                    Demo
-                  </button>
-                </a>
-                <a href="https://github.com/dspedroborges/crudhelper" target="_blank">
-                  <button className="p-2 my-2 rounded-xl bg-gray-900 dark:bg-gray-700 text-white hover:bg-gray-700 dark:hover:bg-gray-600 flex items-center gap-2 justify-center">
-                    Github
-                  </button>
-                </a>
-              </div>
-            </div>
+            {
+              projects.map((p, i) => {
+                return (
+                  <div key={i} className="bg-white dark:bg-gray-800 p-6 rounded shadow transition-colors duration-300">
+                    <h3 className="text-xl font-semibold mb-2 dark:text-gray-200">{p.title}</h3>
+                    <p className="text-gray-600 dark:text-gray-300">
+                      {p.description}
+                    </p>
+                    <div className="flex items-center gap-2 mt-2 flex-wrap">
+                      <a href={p.demo} target="_blank">
+                        <button className="cursor-pointer p-2 my-2 rounded-xl bg-gray-900 dark:bg-gray-700 text-white hover:bg-gray-700 dark:hover:bg-gray-600 flex items-center gap-2 justify-center">
+                          Demo
+                        </button>
+                      </a>
+                      <a href={p.github} target="_blank">
+                        <button className="cursor-pointer p-2 my-2 rounded-xl bg-gray-900 dark:bg-gray-700 text-white hover:bg-gray-700 dark:hover:bg-gray-600 flex items-center gap-2 justify-center">
+                          Github
+                        </button>
+                      </a>
+                    </div>
+                  </div>
+                )
+              })
+            }
           </div>
         </section>
       </main>
